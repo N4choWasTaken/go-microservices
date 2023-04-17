80830a62f90174bbe0f3b2eb1ce1420012c0e66b
N4choWasTaken <thierrygmuer@outlook.com>
2023-04-17 09:58:45 +0200

    :sparkles: implemented heartbeat

--- GoMicroservice.broker/cmd/api/routes.go
+++ GoMicroservice.broker/cmd/api/routes.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
-func routes() {
+func routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(cors.Handler(cors.Options{
@@ -16,4 +19,8 @@ func routes() {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
 }
